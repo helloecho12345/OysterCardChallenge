0841258040d818f15fa9d48c6c77a1129d151643
E Cho <helloecho12345@gmail.com>
2021-12-13 16:12:02 +0000

    add balance and top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  it "has a balance of zero" do  
+   expect(subject.balance).to eq(0)
+  end
+
+  it "adds money to the oystercard" do
+    expect(subject).to respond_to(:top_up) 
+  end
 end
